bfbb12cb3492d057e8335befcea24b51ba517e89
hhajibadri <hhajibadri@ucsd.edu>
2024-05-07 02:01:20 -0700

    fixed path

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -13,23 +13,23 @@ function init() {
   const confetti = new JSConfetti();
   
   audio.volume = 0.5;
-  audioSliderImage.src = 'assets/icons/volume-level-1.svg';
+  audioSliderImage.src = './assets/icons/volume-level-1.svg';
   
   hornSelect.addEventListener('change', function() {
     let selectedHorn = hornSelect.value;
   
     switch(selectedHorn){
       case 'air-horn':
-        img.src = 'assets/images/air-horn.svg';
-        audio.src = 'assets/audio/air-horn.mp3';
+        img.src = './assets/images/air-horn.svg';
+        audio.src = './assets/audio/air-horn.mp3';
         break;
       case 'car-horn':
-        img.src = 'assets/images/car-horn.svg';
-        audio.src = 'assets/audio/car-horn.mp3';
+        img.src = './assets/images/car-horn.svg';
+        audio.src = './assets/audio/car-horn.mp3';
         break;
       case 'party-horn':
-        img.src = 'assets/images/party-horn.svg';
-        audio.src = 'assets/audio/party-horn.mp3';
+        img.src = './assets/images/party-horn.svg';
+        audio.src = './assets/audio/party-horn.mp3';
         break;
     }
   });
@@ -44,16 +44,16 @@ function init() {
 
   audioSlider.addEventListener('input', function() {
     if(audioSlider.value == 0){
-      audioSliderImage.src = 'assets/icons/volume-level-0.svg';
+      audioSliderImage.src = './assets/icons/volume-level-0.svg';
     }
     else if(audioSlider.value < 33){
-      audioSliderImage.src = 'assets/icons/volume-level-1.svg';
+      audioSliderImage.src = './assets/icons/volume-level-1.svg';
     }
     else if(audioSlider.value < 67){
-      audioSliderImage.src = 'assets/icons/volume-level-2.svg';
+      audioSliderImage.src = './assets/icons/volume-level-2.svg';
     }
     else{
-      audioSliderImage.src = 'assets/icons/volume-level-3.svg';
+      audioSliderImage.src = './assets/icons/volume-level-3.svg';
     }
     audio.volume = audioSlider.value / 100;
   });
