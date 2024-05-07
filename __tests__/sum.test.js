bd2444c844504073c2452b350bb8dc817442d6e8
hhajibadri <hhajibadri@ucsd.edu>
2024-05-07 01:43:08 -0700

    Fixed the testing branch

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,11 +2,7 @@
 import { sum } from '../code-to-unit-test/sum';
 
 test('adds 1 + 2 to equal 3', () => {
-<<<<<<< HEAD
   expect(1 + 2).toBe(3);
-=======
-  expect(1 + 2).toBe(4);
->>>>>>> ad043e6d9c7758c153bc5517f08d646189c1c7f6
 });
 
 test('adds 1 + 2 to equal 3', () => {
